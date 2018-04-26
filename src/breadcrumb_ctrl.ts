310c408e70db11c4f16123102c972ac04fcfd71c
zealenth <zealenth@gmail.com>
2018-04-26 13:04:36 -0400

    Prevent the breadcrumbs from clearing query params
    I was helping a friend look at a problem he was experiencing in grafana and I think it might be worth keeping the old query strings around.

--- src/breadcrumb_ctrl.ts
+++ src/breadcrumb_ctrl.ts
@@ -141,7 +141,10 @@ class BreadcrumbCtrl extends PanelCtrl {
              sessionStorage.setItem("dashlist", JSON.stringify(this.dashboardList));
              // Parse modified breadcrumb and set it to url query params
              const parsedBreadcrumb = this.parseBreadcrumbForUrl();
-             this.windowLocation.search({ breadcrumb: parsedBreadcrumb }).replace();
+             this.windowLocation.search({ 
+                 ...this.parseParamsObject(window.location.search),
+                 breadcrumb: parsedBreadcrumb
+             });
          });
      }
 
